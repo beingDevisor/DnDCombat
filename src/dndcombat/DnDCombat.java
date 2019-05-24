7a5279d2bfc2968b5aae6cc17ae53da4d0474184
Mitchell Bath <>
2019-05-24 15:39:55 -0400

    A Dungeons and Dragons application for Dungeon Masters

---
+++ src/dndcombat/DnDCombat.java
@@ -0,0 +1,17 @@
+
+package dndcombat;
+
+/**
+ *
+ * @author Mitchell Bath
+ */
+public class DnDCombat {
+
+    public static void main(String[] args) {
+        Model model = new Model();
+        MainWindow mainWindow = new MainWindow(model);
+        mainWindow.setVisible(true);
+        
+    }
+
+}
